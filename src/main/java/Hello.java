b689006dbdcd10e6dd8a0e3f157392c4f0a9fb11
kdrsyl <kadersayligb@gmail.com>
2020-10-13 18:36:34 +0100

    awesome changes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,17 +9,6 @@ public class Hello {
         System.out.println("new message");
         System.out.println("I made some awesome changes");
         System.out.println("hi all");
-        
-        
-
-        //new things added to feature1
-
-        //feature2 added
-        //new line added to feature2
-
-        //I have a course tomorrow
-        //i will meet with my friends
-        //I studied a lot
 
         System.out.println("today is good");
 
